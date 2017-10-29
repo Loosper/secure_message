6221ae20aed010ddbd1c18c01d6f332c169afe22
Loosper <boian4o1@gmail.com>
2017-10-29 21:43:44 +0200

    Testing heroku

---
+++ app/controllers/submit_controller.rb
@@ -0,0 +1,11 @@
+class SubmitController < ApplicationController
+  def new
+  end
+
+  def create
+    print params
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'submit/create'
+
+  get 'submit/show'
+
+  get 'submit/new'
+
+  get '/', to: 'submit#new'
+  # to displat a specific one
+  get '/submit/:id', to: 'submit#show'
+  post '/submit/', to: 'submit#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
