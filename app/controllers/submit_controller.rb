0b27cb14810b5bfd80ed6be656cbf5b08d6e5cf1
Loosper <boian4o1@gmail.com>
2017-10-31 16:36:00 +0200

    Heroku work pls

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -6,11 +6,15 @@ class SubmitController < ApplicationController
   end
 
   def create
+    # TODO: THERE MIGHT BE SLAAHES! ESCAPE THEM
     @path = Digest::SHA1.base64digest(params[:message])
     Message.new(
       text: params[:message],
       link: @path
     ).save
+
+    # render plain: message.id
+    # redirect_to action: 'show'
     # @path = Digest::SHA1.base64digest(params[:message])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'api/create'
+
   get '/', to: 'submit#new'
   # to displat a specific one
   get '/message/:id', to: 'message#show'
   post '/submit/', to: 'submit#create'
-
+  post '/notes/api/', to: 'api#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
