ff853d1e79a20908f5a6c5beebf9bde16dd058b5
Loosper <boian4o1@gmail.com>
2017-11-05 16:50:30 +0200

    Done with XML and JSON

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -6,15 +6,33 @@ class SubmitController < ApplicationController
   end
 
   def create
-    # TODO: THERE MIGHT BE SLAAHES! ESCAPE THEM
+    if is_xml?
+      params[:message] = Nokogiri::XML(request.body.read).xpath('//message').text
+    end
+
+    # TODO: THERE MIGHT BE SLASHES! ESCAPE THEM
     @path = Digest::SHA1.base64digest(params[:message])
     Message.new(
       text: params[:message],
       link: @path
     ).save
 
-    if request.env['PATH_INFO'] == '/notes/api'
-      render plain: request.base_url + '/message/' + @path
+    @response = {'url': request.base_url + '/message/' + @path}
+
+    respond_to do |format|
+      if self.is_xml?
+        format.xml {render}
+      elsif self.is_json?
+        format.json {render}
+      end
     end
+  end
+
+  def is_xml?
+    request.content_type.include? 'xml'
+  end
+
+  def is_json?
+    request.content_type.include? 'json'
   end
 end
